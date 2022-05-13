d2ee5334cf15d78d635e96a9433072178692cfbb
mehmetuyanikk <94408908+mehmetuyanikk@users.noreply.github.com>
2022-05-13 18:43:22 +0300

    ColonyProjectv1

--- Assets/Scripts/LoginMain.cs
+++ Assets/Scripts/LoginMain.cs
@@ -10,8 +10,11 @@ public class LoginMain : MonoBehaviour
     [SerializeField] InputField _emailRegister, _passwordRegister, _usernameRegister, _repeatPasswordRegister;
     [SerializeField] InputField _passwordLogin, _usernameAndEmailLogin;
     [SerializeField] Toggle _rememberToggle;
+    [SerializeField] Button _registerOrLogin;
     [Header ("Guest Login Settings")]
     [SerializeField] bool _guestLogin;
+    [SerializeField] GameObject _emailRegisterGO, _passwordRegisterGO, _usernameRegisterGO, _repeatPasswordRegisterGO, _passwordLoginGO, _usernameAndEmailLoginGO;
+    [SerializeField] Text _registerOrLoginText;
 
     void LoginEmail()
     {
@@ -40,12 +43,42 @@ public class LoginMain : MonoBehaviour
 
     public void RememberMe()
     {
-
+        if (_rememberToggle.isOn)
+        {
+            PlayerPrefs.SetString("emailOrUsername", _usernameAndEmailLogin.text);
+            PlayerPrefs.SetString("password", _passwordLogin.text);
+        }
     }
 
     public void PlayGuest()
     {
         PlayFabClientAPI.LoginWithAndroidDeviceID(new LoginWithAndroidDeviceIDRequest() { CreateAccount = _guestLogin }, Result => { }, Error => { });
+    }
+
+    public void SwitchLoginOrRegister()
+    {
+        switch (_emailRegisterGO.activeInHierarchy)
+        {
+            case true:
+                _usernameAndEmailLoginGO.SetActive(false);
+                _passwordLoginGO.SetActive(false);
+                _emailRegisterGO.SetActive(true);
+                _passwordRegisterGO.SetActive(true);
+                _repeatPasswordRegisterGO.SetActive(true);
+                _usernameRegisterGO.SetActive(true);
+                _registerOrLoginText.text = "Register";
+                break;
+
+            default:
+                _usernameAndEmailLoginGO.SetActive(true);
+                _passwordLoginGO.SetActive(true);
+                _emailRegisterGO.SetActive(false);
+                _passwordRegisterGO.SetActive(false);
+                _repeatPasswordRegisterGO.SetActive(false);
+                _usernameRegisterGO.SetActive(false);
+                _registerOrLoginText.text = "Login";
+                break;
+        }
     }
 
 }
